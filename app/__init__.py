8ef153dee90ef273a446c32dcbb15a9f15353f61
Ms Eko <jomaduagwu@gmail.com>
2023-08-29 15:38:39 -0500

    setup homepage routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from app.routes import home
 
 def create_app(test_config=None):
   # set up app config
@@ -10,5 +11,8 @@ def create_app(test_config=None):
   @app.route('/hello')
   def hello():
     return 'hello world'
+  
+  # register routes
+  app.register_blueprint(home)
   
   return app
